4f13658ba334a75e1b8f2e3a55f89e5c5edca949
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-11-20 16:45:00 +0100

    feat(setup-project) : install vite-plugin-pages

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import './assets/scss/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(
+    createRouter({
+      history: createWebHistory(),
+      routes
+    })
+  )
 
 app.mount('#app')
