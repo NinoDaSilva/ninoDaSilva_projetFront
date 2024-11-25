96511e10cabe1574d9f06d42ad95b361a3eead58
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-11-25 13:18:34 +0100

    fix(update index) : delete header & footer import

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,7 @@
 <script lang="ts" setup>
-import MyHeader from '@/components/MyHeader.vue';
-import MyFooter from '@/components/MyFooter.vue';
 </script>
 
 <template>
-    <MyHeader />
     <main>
     </main>
-    <MyFooter />
 </template>
