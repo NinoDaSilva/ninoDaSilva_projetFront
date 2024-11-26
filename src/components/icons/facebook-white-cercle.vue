4c052a746104b4f96a42bac7027adce4cca6251b
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-11-26 08:35:48 +0100

    feat(import icons) : import socials icons

---
+++ src/components/icons/facebook-white-cercle.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg id="Calque_2" data-name="Calque 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 73.58 73.58">
+    <g id="Modif">
+      <g id="facebook">
+        <path d="m44.93,24.17h3.77v-8.2h-5.67c-8.04,0-11.85,4.78-11.85,11.59v6.45h-6.29v7.79h6.29v19.81h9.44v-19.81h6.29l1.79-7.79h-8.08v-5.32c0-2.18,1.19-4.53,4.32-4.53Z" fill="#fff"/>
+        <circle cx="36.79" cy="36.79" r="35.29" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="3"/>
+      </g>
+    </g>
+  </svg>
+</template>
