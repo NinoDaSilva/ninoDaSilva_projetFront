6f6b1d62b52baa162ff2a856e5f60b4776429ada
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-11-27 09:30:04 +0100

    feat(creat storybook) : create AuthForm storybook

---
+++ src/stories/AuthForm.stories.ts
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import AuthForm from '@/components/AuthForm.vue'
+
+const meta: Meta<typeof AuthForm> = {
+  component: AuthForm,
+  argTypes: {
+    isSignUp: {
+      control: { type: 'boolean'},
+      description: 'SignUp or SignIn modes',
+      defaultValue: false,
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof AuthForm>
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { AuthForm },
+    setup() {
+      return { args }
+    },
+    template: '<AuthForm v-bind="args" />',
+  }),
+  args: {
+    isSignUp: false,
+  }
+}
+
+export const SignUp: Story = {
+    render: (args) => ({
+      components: { AuthForm },
+      setup() {
+        return { args }
+      },
+      template: '<AuthForm v-bind="args" />',
+    }),
+    args: {
+      isSignUp: true,
+    }
+  }
