cc55d7a15290bb194a340a15865f6a7a48b1536c
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-02 16:12:43 +0100

    fix(delete storybook) : error install storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/storybook'],
+  modules: ['@nuxt/eslint'],
   css: ['@/assets/scss/main.scss'],
   components: [
     { path: '~/components/', pathPrefix: false },
