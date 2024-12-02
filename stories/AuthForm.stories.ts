b60baca9b973d17959d5b1f02eed5df6ee1f630b
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-02 16:02:32 +0100

    feat(install storybook)

--- stories/AuthForm.stories.ts
+++ stories/AuthForm.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/vue3'
+import type { Meta, StoryObj } from '@storybook-vue/nuxt'
 
 import AuthForm from '@/components/AuthForm.vue'
 
--- stories/FormInput.stories.ts
+++ stories/FormInput.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/vue3'
+import type { Meta, StoryObj } from '@storybook-vue/nuxt'
 
 import FormInput from '@/components/FormInput.vue'
 
