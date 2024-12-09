bbc80d4509977d1dbc4d090e52ab57f749773bc3
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-09 15:05:17 +0100

    feat(homepage data) : call homepage sanity data

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <script lang="ts" setup>
+import type { SanityDocument } from "@sanity/client"
+
+const { data: homepage } = await useSanityQuery<SanityDocument>(groq`*[
+_type == "homepage"][0]`);
+
+useSeoMeta({
+    title: 'Accueil | Habits app',
+    description: 'Configurez, organisez et gérer toutes vos habitudes grâce à notre application !',
+    ogTitle: 'Accueil | Habits app',
+    ogDescription: 'Configurez, organisez et gérer toutes vos habitudes grâce à notre application !',
+    ogImage: '/'
+})
 </script>
 
 <template>
-    <main/>
 </template>
