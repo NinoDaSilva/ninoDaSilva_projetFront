99fac869f354a3b2c9ce1716ac81524b3f9fe918
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-10 16:20:35 +0100

    fix(dashboard)

--- pages/app/dashboard.vue
+++ pages/app/dashboard.vue
@@ -1,49 +1,60 @@
 <script setup lang="ts">
-const data = ref();
+import addHabitsForm from '~/components/addHabitsForm.vue';
+
 const error = ref<string>('');
 const router = useRouter();
 
 const token = useCookie('api_tracking_jwt').value;
 
-onMounted(async () => {
+const { data, refresh } = await useAsyncData('dashboard', async () => {
     if (!token) {
         error.value = `Connectez-vous pour accéder aux données !`;
         // Attendre un court délais avant de rediriger vers la page de connexion
         setTimeout(() => {
             router.push('/login');
-        }, 5000);
+        }, 4000);
         return;
     }
 
-    try {
-        const response = await fetch('http://localhost:4000/dashboard', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            },
-        });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || 'Erreur lors du chargement des données.');
-        }
+    const response = await fetch('http://localhost:4000/dashboard', {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
 
-        data.value = await response.json();
-        console.log(data.value)
-    } catch (err) {
-        console.log(err);
-        error.value = 'Une erreur est survenue.';
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erreur lors du chargement des données.');
     }
-});
+
+    return await response.json();
+
+})
+
+async function onHabitCreate() {
+    refresh()
+}
 </script>
 
 <template>
     <div>
         <h1>Dashboard</h1>
 
-        <!-- <div v-if="data">
-            <p>Bienvenue, {{ data.username }}!</p>
-        </div> -->
+        <div v-if="data">
+            <p>Habitudes générales</p>
+            <ul>
+                <li v-for="item of data.globalHabits" :key="item.id">{{ item.title }} : {{ item.description }}</li>
+            </ul>
+
+            <p>Habitudes personnelles</p>
+            <ul>
+                <li v-for="item of data.personalHabits" :key="item.id">{{ item.title }} : {{ item.description }}</li>
+            </ul>
+
+            <AddHabitForm @habit:create="onHabitCreate()" />
+        </div>
 
         <div v-if="error">
             <p>{{ error }}</p>
@@ -51,6 +62,10 @@ onMounted(async () => {
 
         <div v-else>
             <p>Chargement...</p>
+        </div>
+
+        <div>
+            <addHabitsForm />
         </div>
     </div>
 </template>
