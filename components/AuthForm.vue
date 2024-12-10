a4190908f8c3414c7c1af9ce6bf1f01b7dd08f71
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-10 11:18:24 +0100

    fix(add required) : add requierd condition for form input

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { errorMessages } from 'vue/compiler-sfc';
 import Cbutton from './Cbutton.vue';
 
 const props = defineProps<{
@@ -57,13 +56,13 @@ async function onSubmit(event: Event) {
         <h1 class="auth-form__title">{{ isSignUp ? 'Inscription' : 'Connexion' }}</h1>
         <form class="auth-form__form" @submit="onSubmit">
             <FormInput label="Nom d'utilisateur" type="text" name="username" v-model="username"
-                placeholder="Entrez votre nom d'utilisateur" />
+                placeholder="Entrez votre nom d'utilisateur" required />
             <FormInput v-if="isSignUp" label="Adresse email" type="email" name="email" v-model="email"
-                placeholder="Entrez votre email" />
+                placeholder="Entrez votre email" required />
             <FormInput label="Mot de passe" type="password" name="password" v-model="password"
-                placeholder="Entrez votre mot de passe" />
+                placeholder="Entrez votre mot de passe" required />
             <FormInput v-if="isSignUp" label="Confirmer le mot de passe" type="password" name="confirmPassword"
-                v-model="password" placeholder="Confirmez votre mot de passe" />
+                v-model="password" placeholder="Confirmez votre mot de passe" required />
 
             <!-- Gestion des erreurs -->
             <div v-if="error" class="auth-form__error">
