1e717a912afee745ea41ba97db172ecf9906b881
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-11 18:58:17 +0100

    feat(hero style) : add scss for hero

--- pages/index.vue
+++ pages/index.vue
@@ -33,15 +33,63 @@ useSeoMeta({
         buttonVariant="primary" :title="homepage.hero.title" :text="homepage.hero.text"
         :backgroundImg="backgroundImgUrl">
         <template #stats>
-            <ul v-if="homepage.hero.stats" class="hero-stats__list">
-                <li class="hero-stats__item" v-for="stat in homepage.hero.stats">
-                    <span class="hero-stats__value">{{ stat.value }}</span>
-                    <p class="hero-stats__label">{{ stat.text }}</p>
-                </li>
-            </ul>
+            <div class="hero-stats">
+                <ul v-if="homepage.hero.stats" class="hero-stats__list">
+                    <li class="hero-stats__item" v-for="stat in homepage.hero.stats">
+                        <span class="hero-stats__value">{{ stat.value }}</span>
+                        <p class="hero-stats__label">{{ stat.text }}</p>
+                    </li>
+                </ul>
+            </div>
         </template>
     </Hero>
 </template>
 
 <style lang="scss">
+.hero-stats {
+    padding: 5%;
+    color: $PrimaryDark;
+    font-family: $SecondaryFont;
+    max-width: 1000px;
+    margin-inline: auto;
+    @include medium-up {
+        padding: 2%;
+    }
+
+    &__list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        justify-content: center;
+        gap: 5%;
+        text-align: center;
+        @include xsmall-down {
+            grid-template-columns: 1fr;
+        }
+    }
+
+    &__item {
+        margin: rem(10px);
+        @include small-up {
+            margin: rem(10px);
+        }
+    }
+
+    &__value {
+        font-size: rem(25px);
+        font-weight: bold;
+        @include small-up {
+            font-size: rem(35px);
+        }
+    }
+
+    &__label {
+        font-size: rem(14px);
+        color: $InkBase;
+        margin-top: rem(5px);
+        @include small-up {
+            font-size: rem(20px);
+            margin-top: rem(10px);
+        }
+    }
+} 
 </style>
