e3b90b69707c24ed1d5cad8a957f2eab7a039b90
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-11 16:08:13 +0100

    fix(import img) : import sanity img for hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup>
 import type { SanityDocument } from "@sanity/client"
 import Hero from "@/components/Hero.vue";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const { data: homepage } = await useSanityQuery<SanityDocument>(groq`*[
 _type == "homepage"][0]`);
+
+// Sanity Image
+const { projectId, dataset } = useSanity().client.config();
+
+const urlFor = (source: SanityImageSource) =>
+    projectId && dataset
+        ? imageUrlBuilder({ projectId, dataset }).image(source).url()
+        : null;
+
+const backgroundImgUrl = computed(() =>
+    homepage.value?.hero?.backgroundImg ? urlFor(homepage.value.hero.backgroundImg) : "https://placehold.co/600x400"
+);
 
 useSeoMeta({
     title: 'Accueil | Habits app',
@@ -15,5 +29,7 @@ useSeoMeta({
 </script>
 
 <template>
-    <Hero v-if="homepage" :showButton="true" buttonLabel="Get started !" buttonVariant="primary" :title="homepage.hero.title" :text="homepage.hero.text" />
+    <Hero v-if="homepage" :showButton="homepage.hero.showButton" :buttonLabel="homepage.hero.buttonLabel"
+        buttonVariant="primary" :title="homepage.hero.title" :text="homepage.hero.text"
+        :backgroundImg="backgroundImgUrl" />
 </template>
