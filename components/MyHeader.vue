d424f0313e691070f7b2657365e4d4312f4a0f84
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-29 16:54:10 +0100

    feat(header) : add burger menu

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,80 +1,173 @@
 <script lang="ts" setup>
-// Simule l'utilisateur connecté
-const user = ref({
-    isLoggedIn: true,
-    name: "John Doe",
-});
-//
+// Contrôle de l'état du menu burger
+const isMenuOpen = ref(false);
+
+// Fonction pour basculer le menu
+const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+};
+
+
 </script>
 
 <template>
     <header class="header">
-        <div class="header__welcome">
-            <p>Bonjour <span v-if="user.isLoggedIn">{{ user.name }}</span></p>
-        </div>
+        <div class="header__container">
+            <!-- Logo -->
+            <!-- <div class=header__logo></div> -->
 
-        <nav class="header__menu">
-            <ul class="menu__list">
-                <li class="menu__item">
-                    <NuxtLink class="menu__link" to="/">Accueil</NuxtLink>
-                </li>
-                <li class="menu__item">
-                    <NuxtLink class="menu__link" to="/habits">Mes habitudes</NuxtLink>
-                </li>
-                <li class="menu__item">
-                    <NuxtLink class="menu__link" to="/new-habit">Nouvelle habitude</NuxtLink>
-                </li>
-            </ul>
-        </nav>
+            <button class="header__burger" :class="{ 'header__burger--open': isMenuOpen }" @click="toggleMenu"
+                aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <nav :class="{ 'header__menu--open': isMenuOpen }" class="header__menu">
+                <ul class="header__list">
+                    <li class="header__item">
+                        <NuxtLink class="header__link" to="/app/dashboard">Tableau de bord</NuxtLink>
+                    </li>
+                    <li class="header__item">
+                        <NuxtLink class="header__link" to="/app/dashboard">Mes habitudes</NuxtLink>
+                    </li>
+                    <li class="header__item">
+                        <NuxtLink class="header__link" to="/login">Mon espace</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
 <style lang="scss">
 .header {
-    display: flex;
-    align-items: baseline;
-    justify-content: space-between;
-    padding: rem(5px) rem(20px);
-    background-color: $SkyWhite;
-    font-size: rem(20px);
+    background-color: $PrimaryBase;
+    box-sizing: border-box;
+    box-shadow: 0 0 6px $PrimaryDark;
+    position: relative;
 
-    @include large-up() {
-        padding: rem(10px) rem(50px);
+    &__container {
+        display: flex;
+        justify-content: end;
+        align-items: center;
+        padding: rem(18px) 5%;
+        margin: 0 auto;
     }
 
-    &__welcome {
-        font-weight: bold;
+    &__burger {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: rem(26px);
+        height: rem(19px);
+        background: none;
+        border: none;
+        cursor: pointer;
+        z-index: 1001;
 
-        @include medium-up() {
-            font-size: rem(25px);
+        @include medium-up {
+            display: none;
         }
 
         span {
-            color: $PrimaryDark;
+            display: block;
+            height: 3px;
+            width: 100%;
+            border-radius: 50px;
+            background: $SkyWhite;
+            transition: all 0.3s ease;
+        }
+
+        &--open span:nth-child(1) {
+            transform: rotate(45deg) translate(6px, 6px);
+        }
+
+        &--open span:nth-child(2) {
+            opacity: 0;
+        }
+
+        &--open span:nth-child(3) {
+            transform: rotate(-45deg) translate(5px, -5px);
         }
     }
 
     &__menu {
-        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        padding: rem(80px) rem(20px);
+        background-color: $PrimaryDark;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        opacity: 0;
+        visibility: hidden;
+        transform: scale(1.1);
+        transition: all 0.3s ease;
+        z-index: 1000;
 
-        @include medium-up() {
-            display: block;
+        &--open {
+            opacity: 1;
+            visibility: visible;
+            transform: scale(1);
         }
 
-        .menu__list {
-            display: flex;
+        @include medium-up {
+            position: static;
+            padding: rem(10px);
+            background: none;
+            flex-direction: row;
+            opacity: 1;
+            visibility: visible;
+            transform: none;
+        }
+    }
+
+    &__list {
+        display: flex;
+        flex-direction: column;
+        gap: rem(25px);
+
+        @include medium-up {
+            flex-direction: row;
             gap: rem(30px);
-            list-style: none;
+        }
+    }
 
-            .menu__link {
-                text-decoration: none;
-                color: $InkDarker;
-                transition: all 0.2s ease;
+    &__item {
+        font-size: rem(20px);
 
-                &:hover {
-                    color: $PrimaryDark;
-                }
-            }
+        @include medium-up {
+            font-size: rem(18px);
+        }
+    }
+
+    &__link {
+        text-decoration: none;
+        color: white;
+        font-weight: 500;
+        transition: all 0.3s ease;
+        position: relative;
+
+        &:hover {
+            text-shadow: 0 0 2px $SkyWhite;
+        }
+
+        &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: -2px;
+            width: 0;
+            height: 2px;
+            background-color: $PrimaryLighter;
+            transition: width 0.4s ease-in-out;
+        }
+
+        &:hover::after {
+            width: 100%;
         }
     }
 }
