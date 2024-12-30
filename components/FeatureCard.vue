2bb60e454e57f6966a83ed61d79f90b80eb8e067
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-30 16:55:28 +0100

    feat(create card) : create features card template

---
+++ components/FeatureCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+    featureIcon?: string | null,
+    title: string,
+    text: string,
+}>()
+</script>
+
+<template>
+    <div class="feature-card">
+        <img v-if="featureIcon" :src="featureIcon" alt="Feature icon" class="feature-card__img">
+        <h4 class="feature-card__title">{{ title }}</h4>
+        <p class="feature-card__text">{{ text }}</p>
+    </div>
+</template>
