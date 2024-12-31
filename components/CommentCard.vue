2293f0f3b182fd1da795920c7454622400fc9636
NinoDaSilva <nino.da_silva@edu.univ-fcomte.fr>
2024-12-31 12:03:53 +0100

    feat(create template) : create comment card component

---
+++ components/CommentCard.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+    name: string,
+    text: string,
+    work?: string,
+}>()
+</script>
+
+<template>
+    <div class="comment-card">
+        <p class="comment-card__text">{{ text }}</p>
+        <h4 class="comment-card__title">{{ name }}</h4>
+        <p class="comment-card__text --work">{{ work }}</p>
+    </div>
+</template>
